eb4d12308b9488adc92618a6012a5021667c4b2c
TheWeirdDee <divinenation1@gmail.com>
2026-06-06 17:43:09 +0100

    refactor: remove src/app/(app)/followers/page.tsx

--- src/app/(app)/followers/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const FollowersContent = dynamic(() => import('./FollowersContent'), { ssr: false });
-
-export default function FollowersPage() {
-  return <FollowersContent />;
-}
