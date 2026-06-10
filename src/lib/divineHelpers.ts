45f506ab2376a76476140b15607dd2613d0a8043
TheWeirdDee <divinenation1@gmail.com>
2026-06-10 17:58:48 +0100

    chore: delete all junk files and lock them in gitignore

--- src/lib/divineHelpers.ts
+++
@@ -1,7 +0,0 @@
-export function formatAddress(address: string): string {
-  return `${address.slice(0, 6)}…${address.slice(-4)}`;
-}
-
-export function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
